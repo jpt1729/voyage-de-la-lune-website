1e9271a8176166a4023fc3015d17102d94072d3d
John <john.tanaristy@gmail.com>
2024-08-12 00:19:04 -0500

    Cassette player work

---
+++ src/components/cassette-player/index.js
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
+
+import CassettePlayerSvg from "./cassette-player";
+
+export default function CassettePlayer({}) {
+  const audioRef = useRef();
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    if (playing) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    const handleEnded = () => setPlaying(false);
+
+    // Attach the event listener
+    audio.addEventListener("ended", handleEnded);
+
+    // Cleanup the event listener on unmount or when src changes
+    return () => {
+      audio.removeEventListener("ended", handleEnded);
+    };
+  }, [playing, setPlaying]);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setPlaying(!playing);
+        }}
+      >
+        <CassettePlayerSvg
+          className="w-[870px] outline-none"
+          whileHover={{
+            scale: 1.01,
+            transition: { duration: 0.3 },
+          }}
+          whileTap={{ scale: 0.9, transition: { duration: 0.1 } }}
+          playing={playing}
+        />
+        <audio ref={audioRef} src={"/audio.mp3"} className="hidden" />
+      </button>
+    </>
+  );
+}
